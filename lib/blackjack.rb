e8413c29ccffd0c5ceb1bde8fe1769b1e723c696
Paul Hoffart <pahoffart25@gmail.com>
2019-12-18 17:14:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,9 +34,9 @@ def hit?(card_total)
 prompt_user
 user_input = get_user_input
 if user_input == "s"
-    return card_total
+     card_total
   elif user_input == "h"
-    return card_total
+     card_total + deal_card
   else
     puts invalid_command
   end
